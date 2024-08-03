42a2c38d21bca2ecf1e9674c1e912531456ef922
Sergey Slipchenko <faergeek@gmail.com>
2024-08-03 07:31:16 +0400

    Use type parameter in useAppActionData and useAppLoaderData

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -2,10 +2,10 @@ import { useActionData, useLoaderData } from 'react-router-dom';
 
 import type { AppActionFunction, AppLoaderFunction } from './types';
 
-export function useAppActionData<T extends AppActionFunction>(action: T) {
-  return useActionData() as Awaited<ReturnType<typeof action>> | undefined;
+export function useAppActionData<T extends AppActionFunction>(_: T) {
+  return useActionData() as Awaited<ReturnType<T>> | undefined;
 }
 
-export function useAppLoaderData<T extends AppLoaderFunction>(loader: T) {
-  return useLoaderData() as Awaited<ReturnType<typeof loader>>;
+export function useAppLoaderData<T extends AppLoaderFunction>(_: T) {
+  return useLoaderData() as Awaited<ReturnType<T>>;
 }
